2afb04b013e889700a0f41fcbf6ee23330385ef7
Michael Lustfield <michael@lustfield.net>
2012-04-10 10:23:28 -0500

    Initial commit, version 0.1.0 of the plugin.

---
+++ bottle_mysql.py
@@ -0,0 +1,127 @@
+'''
+Bottle-MySQL is a plugin that integrates MySQL with your Bottle
+application. It automatically connects to a database at the beginning of a
+request, passes the database handle to the route callback and closes the
+connection afterwards.
+
+To automatically detect routes that need a database connection, the plugin
+searches for route callbacks that require a `mdb` keyword argument
+(configurable) and skips routes that do not. This removes any overhead for
+routes that don't need a database connection.
+
+Results are returned as dictionaries.
+
+Usage Example::
+
+    import bottle
+    import bottle_mysql
+
+    app = bottle.Bottle()
+    plugin = bottle_mysql.Plugin(dbhost='localhost', dbuser='user', dbpass='pass', dbname='db')
+    app.install(plugin)
+
+    @app.route('/show/:item')
+    def show(item, mdb):
+        mdb.execute('SELECT * from items where name="%s"', (item))
+        row = mdb.fetchone()
+        if row:
+            return template('showitem', page=row)
+        return HTTPError(404, "Page not found")
+'''
+
+__author__ = "Michael Lustfield"
+__version__ = '0.1.0'
+__license__ = 'MIT'
+
+### CUT HERE (see setup.py)
+
+import inspect
+import MySQLdb
+import MySQLdb.cursors as cursors
+from bottle import HTTPResponse, HTTPError
+
+
+class MySQLPlugin(object):
+    '''
+    This plugin passes a mysql database handle to route callbacks
+    that accept a `db` keyword argument. If a callback does not expect
+    such a parameter, no connection is made. You can override the database
+    settings on a per-route basis.
+    '''
+
+    name = 'mysql'
+
+    def __init__(self, dbhost=None, dbuser=None, dbpass=None, dbname=None, autocommit=True, dictrows=True, keyword='mdb'):
+         self.dbhost = dbhost
+         self.dbuser = dbuser
+         self.dbpass = dbpass
+         self.dbname = dbname
+         self.autocommit = autocommit
+         self.dictrows = dictrows
+         self.keyword = keyword
+
+    def setup(self, app):
+        '''
+        Make sure that other installed plugins don't affect the same keyword argument.
+        '''
+        for other in app.plugins:
+            if not isinstance(other, MySQLPlugin):
+                continue
+            if other.keyword == self.keyword:
+                raise PluginError("Found another mysql plugin with conflicting settings (non-unique keyword).")
+
+    def apply(self, callback, context):
+        # Override global configuration with route-specific values.
+        conf = context['config'].get('mysql') or {}
+        dbhost = conf.get('dbhost', self.dbhost)
+        dbuser = conf.get('dbuser', self.dbuser)
+        dbpass = conf.get('dbpass', self.dbpass)
+        dbname = conf.get('dbname', self.dbname)
+        autocommit = conf.get('autocommit', self.autocommit)
+        dictrows = conf.get('dictrows', self.dictrows)
+        keyword = conf.get('keyword', self.keyword)
+
+        # Test if the original callback accepts a 'db' keyword.
+        # Ignore it if it does not need a database handle.
+        args = inspect.getargspec(context['callback'])[0]
+        if keyword not in args:
+            return callback
+
+        def wrapper(*args, **kwargs):
+            # Connect to the database
+            con = None
+            try:
+                # Using DictCursor lets us return result as a dictionary instead of the default list
+                if dictrows:
+                    con = MySQLdb.connect(dbhost, dbuser, dbpass, dbname, cursorclass=cursors.DictCursor);
+                else:
+                    con = MySQLdb.connect(dbhost, dbuser, dbpass, dbname);
+                cur = con.cursor()
+            except HTTPResponse, e:
+                raise HTTPError(500, "Database Error", e)
+
+            # Add the connection handle as a keyword argument.
+            kwargs[keyword] = cur
+
+            try:
+                rv = callback(*args, **kwargs)
+                if autocommit:
+                    con.commit()
+            except MySQLdb.IntegrityError, e:
+                con.rollback()
+                raise HTTPError(500, "Database Error", e)
+            except HTTPError, e:
+                raise
+            except HTTPResponse, e:
+                if autocommit:
+                    con.commit()
+                raise
+            finally:
+                if con:
+                    con.close()
+            return rv
+
+        # Replace the route callback with the wrapped one.
+        return wrapper
+
+Plugin = MySQLPlugin
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from distutils.core import setup
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+# This ugly hack executes the first few lines of the module file to look up some
+# common variables. We cannot just import the module because it depends on other
+# modules that might not be installed yet.
+filename = os.path.join(os.path.dirname(__file__), 'bottle_mysql.py')
+source = open(filename).read().split('### CUT HERE')[0]
+exec(source)
+
+setup(
+    name = 'bottle-mysql',
+    version = __version__,
+    url = 'https://github.com/MTecknology/bottle-mysql',
+    description = 'MySQL integration for Bottle.',
+    long_description = __doc__,
+    author = 'Michael Lustfield',
+    author_email = 'dev@lustfield.net',
+    license = __license__,
+    platforms = 'any',
+    py_modules = [
+        'bottle_mysql'
+    ],
+    requires = [
+        'bottle (>=0.9)'
+    ],
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    cmdclass = {'build_py': build_py}
+)
