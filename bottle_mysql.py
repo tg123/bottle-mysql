5f5808d6337c36e32cb105a2dac07a18951745e4
OloZ17 <glims@wanadoo.fr>
2014-11-18 22:18:47 +0100

    updated to bottle plugin API v2

--- bottle_mysql.py
+++ bottle_mysql.py
@@ -1,4 +1,4 @@
-'''
+"""
 Bottle-MySQL is a plugin that integrates MySQL with your Bottle
 application. It automatically connects to a database at the beginning of a
 request, passes the database handle to the route callback and closes the
@@ -28,10 +28,10 @@ Usage Example::
         if row:
             return template('showitem', page=row)
         return HTTPError(404, "Page not found")
-'''
+"""
 
 __author__ = "Michael Lustfield"
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 __license__ = 'MIT'
 
 ### CUT HERE (see setup.py)
@@ -39,20 +39,30 @@ __license__ = 'MIT'
 import inspect
 import MySQLdb
 import MySQLdb.cursors as cursors
-from bottle import HTTPResponse, HTTPError
+import bottle
+
+
+# PluginError is defined to bottle >= 0.10
+if not hasattr(bottle, 'PluginError'):
+    class PluginError(bottle.BottleException):
+        pass
+
+    bottle.PluginError = PluginError
 
 
 class MySQLPlugin(object):
-    '''
+    """
     This plugin passes a mysql database handle to route callbacks
     that accept a `db` keyword argument. If a callback does not expect
     such a parameter, no connection is made. You can override the database
     settings on a per-route basis.
-    '''
+    """
 
     name = 'mysql'
+    api = 2
 
-    def __init__(self, dbuser=None, dbpass=None, dbname=None, dbhost='localhost', dbport=3306, autocommit=True, dictrows=True, keyword='db', charset='utf8', timezone=None):
+    def __init__(self, dbuser=None, dbpass=None, dbname=None, dbhost='localhost', dbport=3306, autocommit=True,
+                 dictrows=True, keyword='db', charset='utf8', timezone=None):
         self.dbhost = dbhost
         self.dbport = dbport
         self.dbuser = dbuser
@@ -65,33 +75,48 @@ class MySQLPlugin(object):
         self.timezone = timezone
 
     def setup(self, app):
-        '''
+        """
         Make sure that other installed plugins don't affect the same keyword argument.
-        '''
+        """
         for other in app.plugins:
             if not isinstance(other, MySQLPlugin):
                 continue
             if other.keyword == self.keyword:
                 raise PluginError("Found another mysql plugin with conflicting settings (non-unique keyword).")
+            elif other.name == self.name:
+                self.name += '_%s' % self.keyword
 
-    def apply(self, callback, context):
+    def apply(self, callback, route):
+        # hack to support bottle v0.9.x
+        if bottle.__version__.startswith('0.9'):
+            config = route['config']
+            _callback = route['callback']
+        else:
+            config = route.config
+            _callback = route.callback
+
         # Override global configuration with route-specific values.
-        conf = context['config'].get('mysql') or {}
-        dbhost = conf.get('dbhost', self.dbhost)
-        dbport = conf.get('dbport', self.dbport)
-        dbuser = conf.get('dbuser', self.dbuser)
-        dbpass = conf.get('dbpass', self.dbpass)
-        dbname = conf.get('dbname', self.dbname)
-        autocommit = conf.get('autocommit', self.autocommit)
-        dictrows = conf.get('dictrows', self.dictrows)
-        keyword = conf.get('keyword', self.keyword)
-        charset = conf.get('charset', self.charset)
-        timezone = conf.get('timezone', self.timezone)
+        if "mysql" in config:
+            # support for configuration before `ConfigDict` namespaces
+            g = lambda key, default: config.get('mysql', {}).get(key, default)
+        else:
+            g = lambda key, default: config.get('mysql.' + key, default)
+
+        dbhost = g('dbhost', self.dbhost)
+        dbport = g('dbport', self.dbport)
+        dbuser = g('dbuser', self.dbuser)
+        dbpass = g('dbpass', self.dbpass)
+        dbname = g('dbname', self.dbname)
+        autocommit = g('autocommit', self.autocommit)
+        dictrows = g('dictrows', self.dictrows)
+        keyword = g('keyword', self.keyword)
+        charset = g('charset', self.charset)
+        timezone = g('timezone', self.timezone)
 
         # Test if the original callback accepts a 'db' keyword.
         # Ignore it if it does not need a database handle.
-        args = inspect.getargspec(context['callback'])[0]
-        if keyword not in args:
+        _args = inspect.getargspec(_callback)
+        if keyword not in _args.args:
             return callback
 
         def wrapper(*args, **kwargs):
@@ -100,14 +125,15 @@ class MySQLPlugin(object):
             try:
                 # Using DictCursor lets us return result as a dictionary instead of the default list
                 if dictrows:
-                    con = MySQLdb.connect(dbhost, dbuser, dbpass, dbname, dbport, cursorclass=cursors.DictCursor, charset=charset, use_unicode=True);
+                    con = MySQLdb.connect(dbhost, dbuser, dbpass, dbname, dbport, cursorclass=cursors.DictCursor,
+                                          charset=charset, use_unicode=True)
                 else:
-                    con = MySQLdb.connect(dbhost, dbuser, dbpass, dbname, dbport, charset=charset);
+                    con = MySQLdb.connect(dbhost, dbuser, dbpass, dbname, dbport, charset=charset)
                 cur = con.cursor()
                 if timezone:
-                    cur.execute("set time_zone=%s", (timezone, ));
-            except HTTPResponse, e:
-                raise HTTPError(500, "Database Error", e)
+                    cur.execute("set time_zone=%s", (timezone, ))
+            except bottle.HTTPResponse, e:
+                raise bottle.HTTPError(500, "Database Error", e)
 
             # Add the connection handle as a keyword argument.
             kwargs[keyword] = cur
@@ -118,10 +144,10 @@ class MySQLPlugin(object):
                     con.commit()
             except MySQLdb.IntegrityError, e:
                 con.rollback()
-                raise HTTPError(500, "Database Error", e)
-            except HTTPError, e:
+                raise bottle.HTTPError(500, "Database Error", e)
+            except bottle.HTTPError as e:
                 raise
-            except HTTPResponse, e:
+            except bottle.HTTPResponse as e:
                 if autocommit:
                     con.commit()
                 raise
@@ -132,5 +158,6 @@ class MySQLPlugin(object):
 
         # Replace the route callback with the wrapped one.
         return wrapper
+
 
 Plugin = MySQLPlugin
